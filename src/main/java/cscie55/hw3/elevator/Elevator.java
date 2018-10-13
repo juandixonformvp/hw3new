7fa6c2d048db234bc81435bb0f9e09db6dd36615
Robert Chang <robccorp@gmail.com>
2018-10-12 22:42:36 -0400

    worked on Passenger class

--- src/main/java/cscie55/hw3/elevator/Elevator.java
+++ src/main/java/cscie55/hw3/elevator/Elevator.java
@@ -1,7 +1,5 @@
 package cscie55.hw3.elevator;
 
-
-
 /**
  * A <code>Elevator</code> object represents an Elevator in
  * a building. 
--- src/main/java/cscie55/hw3/elevator/Passenger.java
+++ src/main/java/cscie55/hw3/elevator/Passenger.java
@@ -8,6 +8,36 @@ package cscie55.hw3.elevator;
  */
 
 public class Passenger {
-	
-	
+    private int currentFloor;
+    private int destinationFloor;
+
+    public Passenger(int id) {
+
+    }
+
+    public int getCurrentFloor() {
+        return this.currentFloor;
+
+    }
+
+    public int getDestinationFloor() {
+        return this.destinationFloor;
+
+    }
+
+    public void waitForElevator(int newDestinationFloor) {
+
+    }
+
+    public void boardElevator() {
+
+    }
+
+    public void arrive() {
+
+    }
+
+    public String toString(){
+        return "Floor: "+getCurrentFloor()+" Destination: "+getDestinationFloor();
+    }
 }
