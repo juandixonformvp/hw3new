e2bdece5f9f804cd687c2b7b3275f51eefd4821b
Robert Chang <robccorp@gmail.com>
2018-10-14 20:50:41 -0400

    animal class getters and setters

--- src/main/java/cscie55/hw3/zoo/animals/Animal.java
+++ src/main/java/cscie55/hw3/zoo/animals/Animal.java
@@ -3,10 +3,53 @@ package cscie55.hw3.zoo.animals;
 import java.util.*;
 
 public abstract class Animal {
+
     private String name;
     private int age;
     private boolean male;
     ArrayList<String> favoriteFoods = new ArrayList<String>();
     public enum Diet { VEGGIE, MEAT, OMNI };
     Diet diet;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isMale() {
+        return male;
+    }
+
+    public void setMale(boolean male) {
+        this.male = male;
+    }
+
+    public ArrayList<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(ArrayList<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public Diet getDiet() {
+        return diet;
+    }
+
+    public void setDiet(Diet diet) {
+        this.diet = diet;
+    }
+
+
 }
