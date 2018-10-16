0d89d228050600684bf4b65680c2f8a2c1730e0a
Robert Chang <robccorp@gmail.com>
2018-10-15 21:16:48 -0400

    first Animal test

--- src/test/java/cscie55/hw3/zoo/animals/AnimalTest.java
+++ src/test/java/cscie55/hw3/zoo/animals/AnimalTest.java
@@ -6,15 +6,20 @@ import java.util.Arrays;
 import java.util.HashSet;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import java.util.*;
 
 public class AnimalTest {
     @Test
     public void animalPrint() {
 
         Animal lion1 = new Lion();
-        Animal lion2 = new Lion("liono",12, false);
+        Animal lion2 = new Lion("Liono",12, false);
+        ArrayList<String> lionFoods = new ArrayList<String>();
+        lionFoods.add("beef");
+        Animal lion3 = new Lion("King",8, true, lionFoods, Animal.Diet.MEAT);
         System.out.println(lion1.toString());
         System.out.println(lion2.toString());
+        System.out.println(lion3.toString());
     }
 
 }
